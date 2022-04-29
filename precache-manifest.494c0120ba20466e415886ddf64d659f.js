da2161f6a6a2ebca45cd1cedc9757779531f9867
leva <levlevlavrov@gmail.com>
2022-04-29 15:21:33 +0300

    Updates

---
+++ precache-manifest.494c0120ba20466e415886ddf64d659f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "449ed084bb4bd3b48a9c",
+    "url": "/YandexCourseReact/static/css/main.678e3359.chunk.css"
+  },
+  {
+    "revision": "449ed084bb4bd3b48a9c",
+    "url": "/YandexCourseReact/static/js/main.449ed084.chunk.js"
+  },
+  {
+    "revision": "656a7a34132972d3254c",
+    "url": "/YandexCourseReact/static/js/1.656a7a34.chunk.js"
+  },
+  {
+    "revision": "b4806b0d29da767232c0",
+    "url": "/YandexCourseReact/static/css/2.f58dc67f.chunk.css"
+  },
+  {
+    "revision": "b4806b0d29da767232c0",
+    "url": "/YandexCourseReact/static/js/2.b4806b0d.chunk.js"
+  },
+  {
+    "revision": "2e7236a558c8738a235e",
+    "url": "/YandexCourseReact/static/js/runtime~main.2e7236a5.js"
+  },
+  {
+    "revision": "8a95b9f3167630fe43a63e43a83e5b2e",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-ExtraLight.8a95b9f3.woff2"
+  },
+  {
+    "revision": "3227c83538dc1dc1f57e1954b437e0d8",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Italic.3227c835.woff2"
+  },
+  {
+    "revision": "846172004976073c0aff81d4eac924de",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Bold.84617200.woff2"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "/YandexCourseReact/static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "/YandexCourseReact/static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "/YandexCourseReact/static/media/iceland.df042be2.woff"
+  },
+  {
+    "revision": "43cc48dde0a2e7d20ad35add42c6f074",
+    "url": "/YandexCourseReact/index.html"
+  }
+];
